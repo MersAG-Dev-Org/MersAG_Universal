593388698b0bd402619152221a902f32b3f4a2f9
johnmholmes <60571002+johnmholmes@users.noreply.github.com>
2026-08-13 19:14:52 +0100

    Update Config.h

--- Universal/Universal/Config.h
+++ Universal/Universal/Config.h
@@ -11,7 +11,7 @@
 
 // To Force Reset EEPROM to Factory Defaults set this value to 1, else 0.
 // Need to do this at least once.
-#define RESET_TO_FACTORY_DEFAULTS 0
+#define RESET_TO_FACTORY_DEFAULTS 1
 
 // Allow direct to JMRI via USB, without CAN controller, comment out for CAN
 
